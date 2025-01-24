ad31ea5fa4204af926fca6722ab251d48f24807d
VaibhavKambar7 <vaibhavkambar123@gmail.com>
2025-01-25 00:21:54 +0530

    moved authoption to util

---
+++ src/lib/authOptions.ts
@@ -0,0 +1,39 @@
+import { PrismaClient } from "@prisma/client";
+import { type NextAuthOptions } from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
+
+const prisma = new PrismaClient();
+
+export const authOptions: NextAuthOptions = {
+  session: {
+    strategy: "jwt",
+  },
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+    }),
+  ],
+  callbacks: {
+    async signIn({ profile }) {
+      if (!profile?.email) {
+        throw new Error("No profile found :(");
+      }
+
+      await prisma.user.upsert({
+        where: {
+          email: profile.email,
+        },
+        create: {
+          email: profile.email,
+          name: profile.name,
+        },
+        update: {
+          name: profile.name,
+        },
+      });
+
+      return true;
+    },
+  },
+};
